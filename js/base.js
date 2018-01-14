ecdfb0b15fb412e7f948f29714b685ab046c424a
Melody <annemelodysy@gmail.com>
2018-01-13 18:02:38 -0800

    Reduced lines of code

--- js/base.js
+++ js/base.js
@@ -1,4 +1,3 @@
-$(".thumb img").css("display","none");
 $(".parallax img").css("display","none");
 var bgHeight = $(window).height();
 var bgWidth = $(window).width();
@@ -9,23 +8,6 @@ $(document).ready(function() {
   //Calculate Min and Max for blasting
   function rand(min, max) {
     return parseInt(Math.round(min + Math.random() * (max - min)));
-  }
-  //Check if journal-cover exists on page and switch cover
-  if ($("#journal-cover").length) {
-    function getBgUrl(el) {
-      var bg = "";
-      if (el.currentStyle) { // IE
-          bg = el.currentStyle.backgroundImage;
-      } else if (document.defaultView && document.defaultView.getComputedStyle) { // Firefox
-          bg = document.defaultView.getComputedStyle(el, "").backgroundImage;
-      } else { // try and get inline style
-          bg = el.style.backgroundImage;
-      }
-      return bg.replace(/url\(['"]?(.*?)['"]?\)/i, "$1");
-    }
-
-    var image = document.createElement('img');
-    image.src = getBgUrl(document.getElementById('journal-cover'));
   }
 
   // Blast.js separate the characters
@@ -38,7 +20,7 @@ $(document).ready(function() {
 
   //Blast characters randomly
   $('.blast').each(function() {
-    $(this).delay(rand(100,1500)).fadeTo(1500,1);
+    $(this).delay(rand(100,1500)).fadeTo(2000,1);
   });
   //Hide text when scrolling
   $(window).scroll(function(){
@@ -46,22 +28,10 @@ $(document).ready(function() {
   });
 
   /////Parallax effect
-  var thumb=$(".thumb img").attr("src");$("#journal-cover").css("background-image","url("+thumb+")"),$(".parallax").each(function(){var a=$(this).find("img").attr("src");$(this).css("background-image","url("+a+")")});
+  $(".parallax").each(function(){var a=$(this).find("img").attr("src");$(this).css("background-image","url("+a+")")});
 
   ////Fog Effect
   function Particle(t){this.x=0,this.y=0,this.xVelocity=0,this.yVelocity=0,this.radius=5,this.context=t,this.draw=function(){return this.image?void this.context.drawImage(this.image,this.x-128,this.y-128):(this.context.beginPath(),this.context.arc(this.x,this.y,this.radius,0,2*Math.PI,!1),this.context.fillStyle="rgba(0, 255, 255, 1)",this.context.fill(),void this.context.closePath())},this.update=function(){this.x+=this.xVelocity,this.y+=this.yVelocity,this.x>=canvasWidth?(this.xVelocity=-this.xVelocity,this.x=canvasWidth):this.x<=0&&(this.xVelocity=-this.xVelocity,this.x=0),this.y>=canvasHeight?(this.yVelocity=-this.yVelocity,this.y=canvasHeight):this.y<=0&&(this.yVelocity=-this.yVelocity,this.y=0)},this.setPosition=function(t,i){this.x=t,this.y=i},this.setVelocity=function(t,i){this.xVelocity=t,this.yVelocity=i},this.setImage=function(t){this.image=t}}function generateRandom(t,i){return Math.random()*(i-t)+t}function init(){var t=document.getElementById("fog");if(t.getContext){context=t.getContext("2d");for(var i=0;particleCount>i;++i){var e=new Particle(context);e.setPosition(generateRandom(0,canvasWidth),generateRandom(0,canvasHeight)),e.setVelocity(generateRandom(-maxVelocity,maxVelocity),generateRandom(-maxVelocity,maxVelocity)),particles.push(e)}}else alert("Please use a modern browser")}function draw(){context.fillStyle="rgba(0, 0, 0, 0.2)",context.fillRect(0,0,400,400),particles.forEach(function(t){t.draw()})}function update(){particles.forEach(function(t){t.update()})}var particles=[],particleCount=400,maxVelocity=2,targetFPS=15,canvasWidth=window.innerWidth,canvasHeight=window.innerHeight,imageObj=new Image;imageObj.onload=function(){particles.forEach(function(t){t.setImage(imageObj)})},imageObj.src="http://annemelody.com/assets/2015/lighthouse/smoke.png";var context;$("#fog").length&&init(),context&&setInterval(function(){update(),draw()},1e3/targetFPS);
-
-  /////Contact Page Overlay
-  $('#trigger-overlay').click(function(){
-    $('.menu').fadeOut(200);
-    $('body').css('overflow','hidden');
-    $('.contact-page').fadeIn(300).css('display', 'flex');
-  });
-  $('.overlay-close').click(function(){
-    $('.contact-page').fadeOut(300);
-    $('body').css('overflow','');
-    $('.menu').fadeIn(200);
-  });
 
   /////Fade
   $.fn.onscreen=function(){var t=$(window),o={top:t.scrollTop(),left:t.scrollLeft()};o.right=o.left+t.width(),o.bottom=o.top+t.height();var e=this.offset();return e.right=e.left+this.outerWidth(),e.bottom=e.top+this.outerHeight(),!(o.right<e.left||o.left>e.right||o.bottom<e.top||o.top>e.bottom)},$(".module").length>0&&$(".module").each(function(){$(this).onscreen()&&$(this).addClass("already-visible")}),$(".block").length>0&&$(".block").each(function(){$(this).onscreen()&&$(this).addClass("fade-visible")}),$(window).scroll(function(){$.fn.onscreen=function(){var t=$(window),o={top:t.scrollTop(),left:t.scrollLeft()};o.right=o.left+t.width(),o.bottom=o.top+t.height();var e=this.offset();return e.right=e.left+this.outerWidth(),e.bottom=e.top+this.outerHeight(),!(o.right<e.left||o.left>e.right||o.bottom<e.top||o.top>e.bottom)},$(".module").length>0&&$(".module").each(function(){$(this).onscreen()&&$(this).addClass("come-in")}),$(".block").length>0&&$(".block").each(function(){$(this).onscreen()&&$(this).addClass("fade-in")}),$(".opacity").each(function(){if($(this).onscreen()){var t=$(this).height(),o=$(this).parent().offset().top-t,e=$(window).scrollTop(),i=(e-o)/t;$(this).css("opacity",i)}}),$(".volume").each(function(t){var o=$(this).find(".audio").get(0);$(this).onscreen(!0)&&$(".icon-volume-up").length>0?o.play():o.pause()})});
